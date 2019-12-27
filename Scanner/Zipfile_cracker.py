62823b9d22f03b5841953492a1d3d7be45ad6050
Zureno <pranshu21freak@gmail.com>
2019-12-27 01:05:30 -0500

    Update Zipfile_cracker.py

--- Scanner/Zipfile_cracker.py
+++ Scanner/Zipfile_cracker.py
@@ -9,4 +9,21 @@ def extractFile(zFile, password):
       pass
   def main():
       parser = optparse.OptionParser("usuage%prog " +\ "-f <zipfile> -d <dictionary>")
-      parser
+      parser.add_option('-f', dest ='zname',type ='string',\help = 'specify zip file')
+      parser.add_option('-d', dest ='dname',type ='string',\help = 'specify dictionary file')
+      ( options.args ) = parser.parse_args()
+      if (option.zname == None) | (options.dname == None):
+          print parser.usage
+          exit(0)
+      else:
+          zname = options.zname
+          dname = options.dname
+      zFile = zipfile.ZipFile(zname)
+      passFile = open(dname)
+      for line in passFile.readlines():
+        password = line.strip('\n')
+        t = Thread(target = extractFile, args=(zFile,password))
+        t.start()
+        
+   if __name__=='__main__'
+        main()
