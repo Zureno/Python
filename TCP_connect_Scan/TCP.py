4e8eb6aefce62b2f4c363c24719ff5ea63b60a90
Zureno <pranshu21freak@gmail.com>
2019-12-27 15:47:24 -0500

    Update TCP.py

--- TCP_connect_Scan/TCP.py
+++ TCP_connect_Scan/TCP.py
@@ -9,3 +9,28 @@ tgtPort = options.tgtPort
 if (tgtHost == None) | (tgtPort == None):
    print parser.usage
    exit(0)
+def connScan(tgtHost, tgtPort):
+   try:
+      connSkt = socket(AF_INET, SOCK_STREAM)
+      connSkt.connect((tgtHost, tgtPort))
+      print '[+]%d/tcp open'% tgtPort
+      connSkt.close()
+   except:
+      print '[-]%d/tcp closed'% tgtPort
+def portScan(tgtHost, tgtPort):
+   try:
+      tgtIp = gethostbyname(tgtHost)
+   except:
+      print "[-] Cannot resolve '%s': Unknown host"%tgtHost
+      return 
+   try:
+      tgtName = gethostbyaddr(tgtIP)
+      print '\n[+] Scan Results for:' + tgtName[0]
+   except:
+      print '\n[+] Scan Results for:' + tgtIP
+   setdefaulttimeout(1)
+   for tgtPort in tgtPorts:
+      print 'Scanning port' + tgtPort
+      connScan (tgtHost, int(tgtPort))
+ if __name__ == '__main__':
+   main()
