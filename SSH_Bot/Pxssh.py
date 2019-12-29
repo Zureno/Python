12db1e9d4310b7fb7079d6a2a51ddb9c334351b6
Zureno <pranshu21freak@gmail.com>
2019-12-29 12:32:30 -0500

    Update Pxssh.py

--- SSH_Bot/Pxssh.py
+++ SSH_Bot/Pxssh.py
@@ -1,1 +1,17 @@
+# Pxssh has the ability to interact with the pre-defined methods for login(), logout(), prompt().
 
+import pxssh
+def send_commands(child,cmd):
+  child.sendline(cmd)
+  child.prompt()
+  print child.before
+def connect(host, user, password):
+  try:
+    child = pxssh.pxssh()
+    child.login(host, user, password)
+    return child
+  except:
+    print '[-] Error Connecting'
+    exit(0)
+ child = connect('127.0.0.1','root','toor')
+ send_command(child, 'cat /etc/shadow | grep root')
